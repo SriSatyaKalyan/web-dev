4914e3cf71445ac03d1ceec8a0b59a182431b0c8
SriSatyaKalyan <satyakalyan5@gmail.com>
2022-03-26 13:23:27 -0500

    Added multipleButtons project

---
+++ tutorial_projects/multipleButtons/app.js
@@ -0,0 +1,25 @@
+const makeRandColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+const buttons = document.querySelectorAll('button');
+
+for(let button of buttons){
+    button.addEventListener('click', function() {
+        // console.log("CLICKED!");
+
+        // button.style.backgroundColor =  makeRandColor();
+        // button.style.color = makeRandColor();
+    })
+
+    button.addEventListener('click', colorize);
+}
+
+function colorize(){
+    this.style.backgroundColor = makeRandColor();
+    this.style.color = makeRandColor();
+}
