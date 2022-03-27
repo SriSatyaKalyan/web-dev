050acce735d40ace194d501a5294f57500905ab8
SriSatyaKalyan <satyakalyan5@gmail.com>
2022-03-26 20:34:30 -0500

    Working version of scoreKeeper

--- tutorial_projects/scoreKeeper/app.js
+++ tutorial_projects/scoreKeeper/app.js
@@ -8,7 +8,7 @@ const p2Display = document.querySelector('#p2Display');
 
 let p1Score = 0;
 let p2Score = 0;
-let winningScore = 5;
+let winningScore = 3;
 let isGameOver = false;
 
 p1Button.addEventListener('click', function(){
@@ -17,8 +17,10 @@ p1Button.addEventListener('click', function(){
         p1Score += 1;
         if(p1Score === winningScore){
             isGameOver = true;
-            p1Display.classList.add('winner');
-            p2Display.classList.add('loser');
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p1Display.textContent = p1Score;
     }
@@ -29,8 +31,10 @@ p2Button.addEventListener('click', function(){
         p2Score += 1;
         if(p2Score === winningScore){
             isGameOver = true;
-            p2Display.classList.add('winner');
-            p1Display.classList.add('loser');
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
         }
         p2Display.textContent = p2Score;
     }
@@ -49,6 +53,8 @@ function reset(){
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
-    p1Display.classList.remove('winner', 'loser');
-    p2Display.classList.remove('winner', 'loser');
+    p1Display.classList.remove('has-text-success', 'has-text-danger');
+    p2Display.classList.remove('has-text-success', 'has-text-danger');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
 }
