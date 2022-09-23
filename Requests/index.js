f2f69493b5a3ef9264a4f4facd834061ef77746e
SriSatyaKalyan <satyakalyan5@gmail.com>
2022-09-22 21:04:28 -0500

    Testing connection

--- Requests/index.js
+++ Requests/index.js
@@ -1,9 +1,46 @@
-const express = require('express')
+const express = require('express');
+const { cp } = require('fs');
 const app = express();
+const path = require('path')
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'lol it is funny'
+    },
+    {
+        username: 'Skyler',
+        comment: 'I am offended'
+    },
+    {
+        username: 'Brian',
+        comment: 'This is savage!!'
+    },
+    {
+        username: 'Challie',
+        comment: 'Please subscribe to my channel'
+    }
+]
+
+app.get('/comments', (req,res) => {
+    res.render('comments/index', {comments})
+})
+
+app.get('/comments/new', (req, res) => {
+    res.render('comments/new')
+})
+
+app.post('/comments', (req,res) => {
+    // console.log(req.body)
+    const {username, comment} = req.body;
+    comments.push({username, comment});
+    res.send("IT WORKED!")
+})
 
 app.listen(3000, () => {
     console.log("ON PORT 3000!")
